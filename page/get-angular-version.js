c5cdfacb0dcf0b7574e2973c09e520cf26902ec0
Ihor Klymenok <igorklymenok@gmail.com>
2020-02-22 08:56:39 +0200

    feat: send message to background

--- page/get-angular-version.js
+++ page/get-angular-version.js
@@ -1,6 +1,26 @@
+/**
+ * Message action types
+ * @typedef {'GOT_ANGULAR_VERSION'} ActionTypes
+ */
+
+
+class Action {
+
+  /**
+   * @param {ActionTypes} type Message type
+   * @param {any} payload Payload of the action
+   */
+  constructor(type, payload) {
+    this.type = type
+    this.payload = payload
+  }
+}
+
 function getAngularVersion() {
   const appRoot = document.getElementsByTagName('app-root').item(0);
   const angularVersion = appRoot.getAttribute('ng-version');
 
   return parseFloat(angularVersion);
 }
+
+browser.runtime.sendMessage(new Action('GOT_ANGULAR_VERSION', getAngularVersion()));
